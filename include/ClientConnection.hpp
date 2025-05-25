67c2164a5e964c7ad5002fb0af000217ca30c1d9
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2025-05-25 15:28:15 +0200

    removed the poll() in clientConnection to comply with project requirements of using poll() only once

--- include/ClientConnection.hpp
+++ include/ClientConnection.hpp
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 13:53:30 by kbolon            #+#    #+#             */
-/*   Updated: 2025/05/21 18:23:14 by kbolon           ###   ########.fr       */
+/*   Updated: 2025/05/25 15:23:47 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,27 @@
 #include <string>
 #include "../include/ServerConfig.hpp"
 
+enum ClientState {
+  READING_HEADERS,
+  READING_BODY,
+  REQUEST_COMPLETE
+};
+
 class ClientConnection {
   private:
-    int         _fd;
-    std::string _message;
+    int               _fd;
+    ClientState       _state;
+    std::vector<char> _buffer;
+    size_t            _contentLength;
+    size_t            _bodyStart;
+    bool              _knownContentLength;
 
   public:
     ClientConnection(int fd);
     ~ClientConnection();
 
-    bool        sendMessage();
-    std::string	getMessage() const;
+    std::string	getRawRequest() const;
     int         getFd() const;
     void        closeConnection();
-    std::string recvFullRequest(int client_fd, const ServerConfig& config);
+    bool        readRequest(const ServerConfig& config);
 };
